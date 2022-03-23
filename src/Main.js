b7f0c1269d7ce4ef04c474edcf09595c4df6bb30
dangho1 <dangho98@gmail.com>
2022-03-23 11:47:26 +0100

    functional but ugly navbar in place

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,9 @@
+
 import { useEffect, useState } from "react";
-
-
+import Navbar from './Navbar/Index';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Information from "./Navbar/Information";
+import Register from "./Navbar/Register";
 
 export default function Main() {
 
@@ -9,6 +12,13 @@ export default function Main() {
             <header className="main_header">
                 <h1>Mina & Juan-Pablos Bröllop</h1>
             </header>
+            <Router>
+                <Navbar />
+                <Routes>
+                <Route path='/information' element={<Information />} />
+                <Route path='/register' element={<Register />} />
+                </Routes>
+            </Router>
         hallo
         </div>
     );
