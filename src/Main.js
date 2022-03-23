db370a770e4efe7ef0d242344994b8ca46c504aa
dangho1 <dangho98@gmail.com>
2022-03-23 11:01:12 +0100

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+
+
+export default function Main() {
+
+    return(
+        <div className="main_container">
+            <header className="main_header">
+                <h1>Mina & Juan-Pablos Bröllop</h1>
+            </header>
+        hallo
+        </div>
+    );
+}
